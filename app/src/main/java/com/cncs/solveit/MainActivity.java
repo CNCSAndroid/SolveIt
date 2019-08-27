93fddaf36f1c60025656b60d0a830358f2fae626
CNCSAndroid <nlcompsolutions@gmail.com>
2019-08-27 18:36:53 +0530

    Toast demo in MainActivity.java

--- app/src/main/java/com/cncs/solveit/MainActivity.java
+++ app/src/main/java/com/cncs/solveit/MainActivity.java
@@ -1,7 +1,11 @@
 package com.cncs.solveit;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button calculateButton = findViewById(R.id.calculate);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
